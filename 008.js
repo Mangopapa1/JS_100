dfa700c9e724626a7134611c450517ef77d94368
Mangopapa1 <92600646+Mangopapa1@users.noreply.github.com>
2021-12-04 19:39:02 +0900

    Add files via upload

---
+++ 008.js
@@ -0,0 +1,12 @@
+var d = {
+    'height':180,
+    'weight':78,
+    'weight':84,
+    'temperature':36,
+    'eyesight':1
+};
+
+console.log(d['weight']);
+
+//다음 출력 값은?
+// 84  뒤에 선언 값으로 출력 됨.
---
+++ 009.js
@@ -0,0 +1,23 @@
+// 데이터
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+// var result = //빈칸을 채워주세요
+
+// console.log(result);
+
+
+// 출력
+// 2019/04/26 11:34:27
+
+
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
+
+//concat사용 예제
+// const result = 'Hello'.concat(' ', 'world', '!');
+
+// document.write(result); // Hello world!
